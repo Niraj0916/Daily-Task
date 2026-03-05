e1bb7225fcae273fe77658950f1d9317fd5b02d3
Niraj Rajesh <nirajr2332@gmail.com>
2026-03-05 12:53:02 +0530

    Day 3: If-Else programs in Python and Go

---
+++ Day 3/go_odd_even.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&num)
+
+	if num%2 == 0 {
+		fmt.Println("The number is Even")
+	} else {
+		fmt.Println("The number is Odd")
+	}
+
+	var a, b int
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&a)
+
+	fmt.Print("Enter second number: ")
+	fmt.Scan(&b)
+
+	if a > b {
+		fmt.Println("Maximum number is:", a)
+	} else {
+		fmt.Println("Maximum number is:", b)
+	}
+}
---
+++ Day 3/python_odd_even.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
+
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Maximum number is:", a)
+else:
+    print("Maximum number is:", b)
